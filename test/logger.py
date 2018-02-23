9a9504cb22a9156e8367880ad8cad550cd40efd3
Anush Jayaraman <ajayaraman@splunk.com>
2018-02-23 14:43:31 -0800

    Adding sample docker log generation. - Build docker container (docker build -t my-container .) - Run container and point it to the plugin (docker run --log-driver=splunk-log-plugin --log-opt splunk-url=https://10.141.65.253:42973 --log-opt splunk-token=00000000-0000-0000-0000-000000000000 --log-opt splunk-insecureskipverify=true --log-opt splunk-format=inline -d my-container)

---
+++ test/logger.py
@@ -0,0 +1,15 @@
+import sys
+import time
+
+while True:
+    sys.stderr.write(time.ctime() + " ERROR_SINGLE_LINE: ")
+    sys.stderr.write("This is a single line\n")
+    sys.stderr.write(time.ctime() + " ERROR_MULTI_LINE: ")
+    with open("myblog.txt", "r") as f:
+        multiline_log = f.read()
+    sys.stderr.write(multiline_log)
+    sys.stderr.write(time.ctime() + " ERROR_PARTIAL_LINE: ")
+    with open("stacktrace.txt", "r") as f:
+        stacktrace_log = f.read()
+    sys.stderr.write(stacktrace_log)
+    time.sleep(60)
