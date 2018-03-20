43124e46ba00031ca9515e38970038fa980a984e
Anush Jayaraman <ajayaraman@splunk.com>
2018-03-20 12:59:53 -0700

    Refactoring after reviews.

--- message_processor.go
+++ message_processor.go
@@ -35,23 +35,37 @@ type tmpBuffer struct {
 	tBuf                      bytes.Buffer
 	bufferTimer               time.Time
 	bufferReset               bool
-	bufferHoldDurationExpired bool
 }
 
-func appendToTempBuffer(p *tmpBuffer, b *logdriver.LogEntry) {
+func (b *tmpBuffer) appendToTempBuffer(l *logdriver.LogEntry) bool {
 	// Add msg to temp buffer and disable buffer reset flag
-	ps, err := p.tBuf.Write(b.Line)
-	p.bufferReset = false
+	ps, err := b.tBuf.Write(l.Line)
+	b.bufferReset = false
 	if err != nil {
 		logrus.WithError(err).WithField("Appending to Temp Buffer with size:", ps).Error(
-			"Error appending to buffer")
+			"Error appending to temp buffer")
+		return false
 	}
+	return true
 }
 
-func resetTempBufferValues(p *tmpBuffer) {
-	p.tBuf.Reset()
-	p.bufferTimer = time.Now()
-	p.bufferHoldDurationExpired = false
+func (b *tmpBuffer) resetTempBuffer() {
+	logrus.Debug("Resetting temp buffer")
+	b.tBuf.Reset()
+	b.bufferTimer = time.Now()
+}
+
+func (b *tmpBuffer) isTempBufferHoldDurationExpired (t time.Time) bool {
+	diff := t.Sub(b.bufferTimer)
+	return diff > tempMsgBufferHoldDuration
+}
+
+func (b *tmpBuffer) isTempBufferLengthExceeded() bool {
+	return tempMsgBufferMaximum < b.tBuf.Len()
+}
+
+func (b *tmpBuffer) shouldTempBufferFlush(t time.Time) bool {
+	return b.isTempBufferLengthExceeded() || b.isTempBufferHoldDurationExpired(t)
 }
 
 /*
@@ -62,8 +76,6 @@ func consumeLog(lf *logPair) {
 	// Initialize temp buffer
 	tmpBuf := &tmpBuffer{
 		bufferTimer:               time.Now(),
-		bufferReset:               false,
-		bufferHoldDurationExpired: false,
 	}
 	// create a protobuf reader for the log stream
 	dec := protoio.NewUint32DelimitedReader(lf.stream, binary.BigEndian, 1e6)
@@ -86,18 +98,12 @@ func consumeLog(lf *logPair) {
 
 		if shouldSendMessage(buf.Line) {
 			// Append to temp buffer
-			appendToTempBuffer(tmpBuf, &buf)
-			// Check for temp buffer timer expiration
-			diff := time.Now().Sub(tmpBuf.bufferTimer)
-			if diff > tempMsgBufferHoldDuration {
-				tmpBuf.bufferHoldDurationExpired = true
-			}
-
-			sendMessage(lf.splunkl, &buf, tmpBuf, lf.info.ContainerID)
-			sendMessage(lf.jsonl, &buf, tmpBuf, lf.info.ContainerID)
-			//temp buffer and values reset
-			if tmpBuf.bufferReset {
-				resetTempBufferValues(tmpBuf)
+			if tmpBuf.appendToTempBuffer(&buf) {
+				// Send message to splunk and json logger
+				sendMessage(lf.splunkl, &buf, tmpBuf, lf.info.ContainerID)
+				sendMessage(lf.jsonl, &buf, tmpBuf, lf.info.ContainerID)
+				//temp buffer and values reset
+				if tmpBuf.bufferReset {tmpBuf.resetTempBuffer()}
 			}
 		}
 		buf.Reset()
@@ -105,25 +111,23 @@ func consumeLog(lf *logPair) {
 }
 
 // send the log entry message to logger
-func sendMessage(l logger.Logger, buf *logdriver.LogEntry, tBuffer *tmpBuffer, containerid string) {
+func sendMessage(l logger.Logger, buf *logdriver.LogEntry, t *tmpBuffer, containerid string) {
 	var msg logger.Message
-	if !buf.Partial || tBuffer.bufferHoldDurationExpired || tempMsgBufferMaximum <= tBuffer.tBuf.Len() {
-		// Only send if partial bit is not set or temp buffer size reached max or temp buffer timer expired
+	// Only send if partial bit is not set or temp buffer size reached max or temp buffer timer expired
+	// Check for temp buffer timer expiration
+	if !buf.Partial || t.shouldTempBufferFlush(time.Now()) {
 		logrus.WithField("id", containerid).WithField("Buffer partial flag should be false:",
-			buf.Partial).WithField("Temp buffer hold duration expired should be true:",
-			tBuffer.bufferHoldDurationExpired).WithField("Temp buffer Length:",
-			tBuffer.tBuf.Len()).Debug("Buffer details")
-		msg.Line = tBuffer.tBuf.Bytes()
+			buf.Partial).WithField("Temp buffer Length:", t.tBuf.Len()).Debug("Buffer details")
+		msg.Line = t.tBuf.Bytes()
 		msg.Source = buf.Source
 		msg.Partial = buf.Partial
 		msg.Timestamp = time.Unix(0, buf.TimeNano)
-		err := l.Log(&msg)
 
-		if err != nil {
+		if err := l.Log(&msg); err != nil {
 			logrus.WithField("id", containerid).WithError(err).WithField("message",
 				msg).Error("Error writing log message")
 		}
-		tBuffer.bufferReset = true
+		t.bufferReset = true
 	}
 }
 
