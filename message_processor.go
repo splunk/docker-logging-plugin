964c090f676f436ffd8dc46f37ee0b3f6582c589
rxie <rxie@splunk.com>
2018-04-11 15:47:48 -0700

    remove everything

--- message_processor.go
+++ message_processor.go
@@ -43,9 +43,9 @@ and send the buffer to splunk logger and json logger
 */
 func (mg messageProcessor) consumeLog(lf *logPair) {
 	// Initialize temp buffer
-	tmpBuf := &partialMsgBuffer{
-		bufferTimer: time.Now(),
-	}
+	// tmpBuf := &partialMsgBuffer{
+	// 	bufferTimer: time.Now(),
+	// }
 	// create a protobuf reader for the log stream
 	dec := protoio.NewUint32DelimitedReader(lf.stream, binary.BigEndian, 1e6)
 	defer dec.Close()
@@ -65,17 +65,17 @@ func (mg messageProcessor) consumeLog(lf *logPair) {
 			dec = protoio.NewUint32DelimitedReader(lf.stream, binary.BigEndian, 1e6)
 		}
 
-		if mg.shouldSendMessage(buf.Line) {
-			// Append to temp buffer
-			if err := tmpBuf.append(&buf); err == nil {
-				// Send message to splunk and json logger
-				mg.sendMessage(lf.splunkl, &buf, tmpBuf, lf.info.ContainerID)
-				mg.sendMessage(lf.jsonl, &buf, tmpBuf, lf.info.ContainerID)
-				//temp buffer and values reset
-				tmpBuf.reset()
-			}
-		}
-		buf.Reset()
+		// if mg.shouldSendMessage(buf.Line) {
+		// 	// Append to temp buffer
+		// 	if err := tmpBuf.append(&buf); err == nil {
+		// 		// Send message to splunk and json logger
+		// 		mg.sendMessage(lf.splunkl, &buf, tmpBuf, lf.info.ContainerID)
+		// 		mg.sendMessage(lf.jsonl, &buf, tmpBuf, lf.info.ContainerID)
+		// 		//temp buffer and values reset
+		// 		tmpBuf.reset()
+		// 	}
+		// }
+		// buf.Reset()
 	}
 }
 
