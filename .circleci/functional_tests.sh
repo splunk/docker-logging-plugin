8cdf0227f97e0fe127824d98d844cbf5a8dadb1f
gp510 <glen@splunk.com>
2020-01-22 15:20:22 -0800

    CI update

--- .circleci/functional_tests.sh
+++ .circleci/functional_tests.sh
@@ -6,10 +6,10 @@ set -e
 echo "Running functional tests..."
 
 # Start the plugin
-/repo/splunk-logging-plugin/rootfs/bin/splunk-logging-plugin &
+/go/src/github.com/splunk/docker-logging-plugin/splunk-logging-plugin/rootfs/bin/splunk-logging-plugin &
 
 echo "Creating virtual env to run functional tests..."
-cd /repo/test
+cd /go/src/github.com/splunk/docker-logging-plugin/test
 pip3 install virtualenv
 virtualenv --python=python3.5 venv
 source venv/bin/activate
@@ -22,5 +22,5 @@ python -m pytest \
 	--splunk-password #{ENV['SPLUNK_PASSWORD']} \
 	--splunk-hec-url #{ENV['SPLUNK_HEC_HOST']}:8088 \
 	--splunk-hec-token #{ENV['SPLUNK_HEC_TOKEN']} \
-	--docker-plugin-path /repo/splunk-logging-plugin/rootfs/bin/splunk-logging-plugin \
-	--fifo-path /repo/pipe
+	--docker-plugin-path /go/src/github.com/splunk/docker-logging-plugin/splunk-logging-plugin/rootfs/bin/splunk-logging-plugin \
+	--fifo-path /go/src/github.com/splunk/docker-logging-plugin/pipe
