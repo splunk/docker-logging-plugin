753a6d866a2d613deda1bc2a5943169e3fde7e36
gp510 <glen@splunk.com>
2020-01-22 17:43:36 -0800

    CI update

--- .circleci/functional_tests.sh
+++ .circleci/functional_tests.sh
@@ -18,10 +18,10 @@ pip install -r requirements.txt
 
 
 python -m pytest \
-	--splunkd-url https://#{ENV['SPLUNK_HEC_HOST']}:8089 \
+	--splunkd-url https://${SPLUNK_HEC_HOST}:8089 \
 	--splunk-user admin \
-	--splunk-password #{ENV['SPLUNK_PASSWORD']} \
-	--splunk-hec-url #{ENV['SPLUNK_HEC_HOST']}:8088 \
-	--splunk-hec-token #{ENV['SPLUNK_HEC_TOKEN']} \
+	--splunk-password ${SPLUNK_PASSWORD} \
+	--splunk-hec-url ${SPLUNK_HEC_HOST}:8088 \
+	--splunk-hec-token ${SPLUNK_HEC_TOKEN} \
 	--docker-plugin-path /home/circleci/repo/docker-logging-plugin/splunk-logging-plugin/rootfs/bin/splunk-logging-plugin \
 	--fifo-path /home/circleci/repo/docker-logging-plugin/pipe
