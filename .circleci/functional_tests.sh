4b382fd04d0af7dcc124b26b1b2a0d7b4422ac19
gp510 <glen@splunk.com>
2020-01-23 21:38:00 -0800

    CI update

--- .circleci/functional_tests.sh
+++ .circleci/functional_tests.sh
@@ -5,24 +5,30 @@ set -e
 
 echo "Running functional tests..."
 
+sudo su
+
 # Start the plugin
-sudo splunk-logging-plugin/rootfs/bin/splunk-logging-plugin &
+splunk-logging-plugin/rootfs/bin/splunk-logging-plugin &
 
 echo "Creating virtual env to run functional tests..."
-pyenv global 3.7.0
 cd test
-pip3 install virtualenv
-virtualenv --python=python3.7 venv
-source venv/bin/activate
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+apt-get update && sudo apt-get upgrade
+apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv install 3.7.0
+pyenv global 3.7.0
 pip install -r requirements.txt
 
 
 python -m pytest \
-	--splunkd-url https://$CI_SPLUNK_HEC_HOST:8089 \
+	--splunkd-url https://$SPLUNK_HEC_HOST:8089 \
 	--splunk-user admin \
-	--splunk-password $CI_SPLUNK_PASSWORD \
-	--splunkd-url https://$CI_SPLUNK_HEC_HOST:8088 \
-	--splunk-hec-token $CI_SPLUNK_HEC_TOKEN \
+	--splunk-password $SPLUNK_PASSWORD \
+	--splunkd-url https://$SPLUNK_HEC_HOST:8088 \
+	--splunk-hec-token $SPLUNK_HEC_TOKEN \
 	--docker-plugin-path /home/circleci/repo/splunk-logging-plugin/rootfs/bin/splunk-logging-plugin \
 	--fifo-path /home/circleci/repo/pipe
 	-p no:warnings -s
