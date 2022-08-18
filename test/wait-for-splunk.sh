2fcf4aee7673d18a239807e845858d1a4416b87d
Steve Wood <stephen.wood4@nhs.net>
2022-08-18 17:31:40 +0100

    Fix waiting for Splunk by checking kvStoreStatus

--- test/wait-for-splunk.sh
+++ test/wait-for-splunk.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 COUNT=0
-output=$(curl -s -u admin:notchangeme -k https://localhost:8089/services/server/info/server-info?output_mode=json | jq -r ".entry[0].content.health_info")
+output=$(curl -s -u admin:notchangeme -k https://localhost:8089/services/server/info/server-info?output_mode=json | jq -r ".entry[0].content.kvStoreStatus")
 
-while [ "$output" != "green" ] && [ $COUNT -lt 12 ]
+while [ "$output" != "ready" ] && [ $COUNT -lt 12 ]
 do
     echo "Waiting for Splunk to be ready (iteration $COUNT)..."
     sleep 10
-    output=$(curl -s -u admin:notchangeme -k https://localhost:8089/services/server/info/server-info?output_mode=json | jq -r ".entry[0].content.health_info")
+    output=$(curl -s -u admin:notchangeme -k https://localhost:8089/services/server/info/server-info?output_mode=json | jq -r ".entry[0].content.kvStoreStatus")
     ((COUNT=COUNT+1))
 done
 
-if [ "$output" = "green" ] ; then
+if [ "$output" = "ready" ] ; then
     echo "Splunk now ready."
 else
     echo "Failed waiting for Splunk"
